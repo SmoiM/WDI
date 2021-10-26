d3441e0ac3d1e81ae7e083f9d3b8661d00db37c9
SmoiM <dmichalik@student.agh.edu.pl>
2021-10-26 14:20:09 +0200

    Zadanie 6 dokończyć

---
+++ Labolatorium_3/zadanie_6.py
@@ -0,0 +1,15 @@
+#Podawanie liczb
+
+liczba1 = int(input("Prosze podac liczbe1: "))
+liczba2 = int(input("Prosze podac liczbe2: "))
+
+# Podane liczby
+# Tylko po co
+# Pewnie po nic
+
+print ("Wprowadzone przez Ciebie liczby to: %d i %d" %(liczba1, liczba2))
+
+if (liczba1 and liczba2 < 0):
+    print ("Jedna liczba jest mniejsza od zera")
+
+else:
