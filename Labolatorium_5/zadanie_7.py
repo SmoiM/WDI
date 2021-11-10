c9dffd098a07886ee7162cb39fc613c43ca01173
SmoiM <dmichalik@student.agh.edu.pl>
2021-11-10 15:22:46 +0100

    zadanie_7 final cutzadanie_7 Final Cut

--- Labolatorium_5/zadanie_7.py
+++ Labolatorium_5/zadanie_7.py
@@ -1,20 +1,20 @@
 liczba = int(input("Podaj liczbę: "))
+#zmienna pomocnicza, ktora pełni funkcję dzielnika
 pom = 1
 
-if liczba < 0:
-    liczba = - liczba
-    while pom <= liczba:
-        dzielnik = liczba / pom
-        if dzielnik - int(dzielnik) == 0:
-            print("+/-", pom)
-            pom += 1
-        else:
-            pom += 1
-else:
-    while pom <= liczba:
-        dzielnik = liczba / pom
-        if dzielnik - int(dzielnik) == 0:
-            print("+/-", pom)
-            pom += 1
-        else:
-            pom += 1
+#wartość bezwzględna z liczby
+liczba = abs(liczba)
+
+while pom <= liczba / 2:
+    #operacja modulo - reszta z dzielenia
+    dzielnik = liczba % pom
+
+    if dzielnik == 0:
+        print(pom)
+        pom += 1
+    else:
+        pom += 1
+
+print(liczba)
+
+
