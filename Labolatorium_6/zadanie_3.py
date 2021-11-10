9e3938eddc6f9247292d50e3237f6850deb3a0f3
SmoiM <dmichalik@student.agh.edu.pl>
2021-11-10 16:15:28 +0100

    zadanie_3 v.2

--- Labolatorium_6/zadanie_3.py
+++ Labolatorium_6/zadanie_3.py
@@ -4,7 +4,12 @@ fib2 = 1
 fib3 = fib1 + fib2
 iloczyn = 0
 
-liczba = int(input("Podaj liczbę: "))
+while True:
+    try:
+        liczba = int(input("Podaj liczbę: ")) 
+        break
+    except:
+        print("Podane wyrażenie nie jest liczbą naturalną. Spróbuj ponownie")
 
 while iloczyn <= liczba:
     iloczyn = fib1 * fib2
@@ -12,4 +17,6 @@ while iloczyn <= liczba:
     fib2=fib3
     fib3 = fib1 + fib2
     if iloczyn == liczba:
-        print("Ta liczna jest iloczynem dwóch dwóch wyrazów ciągu Fibonacciego")
+        print("Ta liczba jest iloczynem dwóch dwóch wyrazów ciągu Fibonacciego")
+    #else:
+    #    print("Ta liczba nie jest iloczynem dwóch dwóch wyrazów ciągu Fibonacciego")
