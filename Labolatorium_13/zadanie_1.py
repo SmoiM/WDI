9672d5930b13ddf05d48bb7818f001710150e48d
SmoiM <dmichalik@student.agh.edu.pl>
2022-01-09 17:02:00 +0100

    Lab_13 + zadanie_1

---
+++ Labolatorium_13/zadanie_1.py
@@ -0,0 +1,31 @@
+# Wykorzystując algorytm Euklidesa napisz funkcję wyznaczającą Największy Wspólny Dzielnik (NWD) wprowadzonych przez użytkownika liczb. 
+# Zwizualizuj w konsoli rekurencyjny sposób działania programu.
+
+# Przykłady NWD(645,363) = 3 NWD(420,1386) = 42 NWD(2891,1589) = 7 NWD(252,231) = 21 NWD(924,1105) = 1
+
+def euklides (a, b, tab):
+    a = abs(a)
+    b = abs(b)
+    if (b > a):
+        cos = b
+        b = a
+        a = cos
+    
+    pom = a // b # dzielenie całkowite
+    c = a - (pom * b) 
+    nwd.append(b)
+    print(a, " = ", a//b, " * ", b, " + ", c)
+
+    a = b 
+    b = c
+   
+    if c == 0:
+        print("Największy wspólny dzielnik to ", tab[-1])
+    else:
+        euklides(a,b,tab)
+
+a = int(input("Proszę podać pierwszą liczbę: "))
+b = int(input("Proszę podać drugą liczbę: "))
+nwd = []
+
+euklides(a, b, nwd)
