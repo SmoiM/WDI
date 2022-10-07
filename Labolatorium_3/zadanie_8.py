02f0d5f01aa281af02bcb3f4f162d7b0b832caf1
SmoiM <dmichalik@student.agh.edu.pl>
2022-10-07 19:39:45 +0200

    Poprawa wygladu

--- Labolatorium_3/zadanie_8.py
+++ Labolatorium_3/zadanie_8.py
@@ -1,7 +1,7 @@
 wysokosc = int(input("Proszę podać wysokość choinki: "))
 rzad = 0
 galaz = "*"
-spacja = wysokosc -1
+spacja = wysokosc - 1
 
 if wysokosc < 0 or wysokosc == 0:
     print("wysokość musi być liczbą całkowitą większą od zera!!!")
@@ -9,17 +9,13 @@ if wysokosc < 0 or wysokosc == 0:
 
 while rzad < wysokosc:
     if rzad == 0:
-        print((spacja-1)*" ", "X"*(2*rzad+1))
+        print((spacja - 1)*" ", "X" * (2 * rzad + 1))
     elif rzad != 0 and rzad != wysokosc - 1:
-        print((spacja-1)*" ", galaz*(2*rzad+1)) 
+        print((spacja - 1) * " ", galaz*(2 * rzad + 1)) 
     else:
-        print(galaz*(2*rzad+1)) 
+        print(galaz * (2 * rzad + 1)) 
 
     rzad += 1
     spacja -= 1
 
-print((rzad-2)*" ","U")
-
-#galaz = "*"
-#linia = 3 * galaz
-#print(linia)
+print((rzad - 2) * " ", "U")
